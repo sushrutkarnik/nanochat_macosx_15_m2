b9a0dd6976dcdfa8ddeaebd7618d2a92cdb9f476
Sushrut Karnik <sushrutkarnik@gmail.com>
2026-03-14 18:05:17 +0100

    turn torch.compile back on now that 2.9.1 is installed

--- scripts/base_train.py
+++ scripts/base_train.py
@@ -246,7 +246,7 @@ def disable_fp8(model):
 # Compile the model
 
 orig_model = model # original, uncompiled model, for saving raw model state_dict and for inference/evaluation (because the shapes may change shape)
-# model = torch.compile(model, dynamic=False) # the inputs to model will never change shape so dynamic=False is safe
+model = torch.compile(model, dynamic=False) # the inputs to model will never change shape so dynamic=False is safe
 
 # -----------------------------------------------------------------------------
 # Scaling laws and muP extrapolations to determine the optimal training horizon, batch size, learning rates, weight decay.
--- scripts/chat_sft.py
+++ scripts/chat_sft.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
             print0(f"Using {name}={arg_val}")
 
     orig_model = model
-    # model = torch.compile(model, dynamic=False)
+    model = torch.compile(model, dynamic=False)
     depth = model.config.n_layer
     num_flops_per_token = model.estimate_flops()
     tokens_per_fwdbwd = args.device_batch_size * args.max_seq_len # tokens per iteration for a single rank
